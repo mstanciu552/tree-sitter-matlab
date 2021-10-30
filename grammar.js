664f1d936ea17366e6875d0c688e1977b14b83dd
mstanciu552 <mstanciu552@gmail.com>
2021-10-30 18:50:29 +0300

    added operation rul

--- grammar.js
+++ grammar.js
@@ -3,16 +3,18 @@ module.exports = grammar({
   name: "matlab",
 
   rules: {
+    // TODO Fix source_file
     source_file: $ => repeat($._statement),
-    expression: $ => seq($.identifier, $._operator, choice($._value, $._math_expression), optional(';')),
-    _statement: $ => choice($.expression, $.function_definition),
+    expression: $ => seq($.identifier, $._eq, choice($._value, $._math_expression), optional(';')),
+    operation: $ => seq($._value, repeat(seq($._operator, $._value))),
+    _statement: $ => choice($.expression, $.function_definition, $.operation),
     _value: $ => choice($._number, $.identifier),
     _math_expression: $ => seq($._value, $._operator, $._value),
     function_definition: $ => choice(seq(
       $._function_keyword,
       $.return_variable,
       optional($._whitespace),
-      $._operator,
+      $._eq,
       optional($._whitespace),
       $.function_name,
       $.parameter_list,
@@ -38,7 +40,8 @@ module.exports = grammar({
       '\n'
     ),
     identifier: $ => new RegExp("[a-zA-Z_]+"),
-    _operator: $ => /[=*+-/%]/,
+    _operator: $ => /[*+-/%]/,
+    _eq: $ => "=",
     _number: $ => /\d+/,
     _function_keyword: $ => 'function',
     _end: $ => "end",
