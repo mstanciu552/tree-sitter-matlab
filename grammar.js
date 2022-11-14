28f9be7bf6c19b66bf6d170a710e91d0a69ee541
Mathias Nielsen <m.immanuel@gmail.com>
2022-11-14 15:35:30 +0100

    add statements

--- grammar.js
+++ grammar.js
@@ -1,6 +1,10 @@
 module.exports = grammar({
   name: 'matlab',
 
+  // extras: ($) => [$.comment, /\s/, $._end_of_line],
+
+  word: ($) => $.identifier,
+
   rules: {
     source_file: ($) =>
       repeat(
@@ -9,19 +13,106 @@ module.exports = grammar({
           $.expression,
           $.function_definition,
           $.function_call,
-          $.structure,
+          $._statements
         )
       ),
 
-    structure: ($) =>
+    _statements: ($) =>
+      choice(
+        $.if_statement,
+        $.for_statement,
+        $.while_statement,
+        $.switch_statement,
+        $.try_statement
+      ),
+
+    if_statement: ($) =>
       prec.right(
         seq(
-          field('structure_keyword', $.structure_keyword),
-          choice($.operation, $.bool, $.expression, $._bool_keywords),
+          "if",
+          optional($._not),
+          field('condition', $._condition),
           optional($.block),
+          optional(repeat1($.elseif_statement)),
+          optional($.else_statement),
           field('end', $.end)
         )
       ),
+
+    elseif_statement: ($) =>
+      prec.right(
+        seq(
+          "elseif",
+          field('condition', $._condition),
+          optional($.block),
+        )
+      ),
+
+    else_statement: ($) =>
+      prec.right(
+        seq(
+          "else",
+          optional($.block),
+        )
+      ),
+
+    for_statement: ($) =>
+      prec.right(
+        seq(
+          "for",
+          optional($._not),
+          $.expression,
+          optional($.block),
+          field('endfor', $.end)
+        )
+      ),
+
+    while_statement: ($) =>
+      prec.right(
+        seq(
+          "while",
+          optional($._not),
+          field('condition', $._condition),
+          optional($.block),
+          field('endwhile', $.end)
+        )
+      ),
+
+    switch_statement: ($) =>
+      prec.right(1,
+        seq(
+          "switch", $.identifier,
+          repeat1($.case_statement),
+          optional($.otherwise_statement),
+          field('endswitch', $.end)
+        )
+      ),
+
+    case_statement: ($) =>
+      prec.right(1,
+        seq("case",
+          field('condition', choice($.identifier, $.number, $.string, $.cell_definition)),
+          optional($.block))
+      ),
+
+    otherwise_statement: ($) =>
+      prec.right(1,
+        seq("otherwise", optional($.block))
+      ),
+
+    try_statement: ($) =>
+      prec.right(1,
+        seq("try",
+          optional($.block),
+          optional($.catch_statement),
+          field('endtry', $.end))),
+
+    catch_statement: ($) =>
+      prec.right(1,
+        seq("catch", optional($.identifier), optional($.block))
+      ),
+
+    _condition: ($) => choice($.operation, $.bool, $._bool_keywords, $.function_call),
 
     function_definition: ($) =>
       prec.right(
@@ -45,11 +136,16 @@ module.exports = grammar({
           choice($._and, $._or, $._diff),
           $.factor,
           optional(seq($._comparator_equal, $.factor)),
-          optional(')')
+          optional(')'),
+          optional($._end_of_line)
         )
       ),
+
     operation: ($) =>
-      prec.right(1, seq(optional($._not), $.factor, $._operator, $.factor)),
+      prec.right(1, seq(
+        // optional($._not), 
+        $.factor, $._operator, $.factor, optional($._end_of_line))),
+
     expression: ($) =>
       prec(
         2,
@@ -61,7 +157,7 @@ module.exports = grammar({
           ),
           $._eq,
           choice($.operation, $.factor, $.vector_definition, $.cell_definition),
-          optional($._semi_colon)
+          optional($._end_of_line)
         )
       ),
 
@@ -81,14 +177,36 @@ module.exports = grammar({
         $.identifier,
         seq('[', repeat1(seq($.identifier, optional(','))), ']')
       ),
-    block: ($) =>
-      prec(3, repeat1(choice(field('comment', $.comment), $.expression, $.structure, $.function_call))),
 
-    structure_keyword: ($) => choice('if', 'for', 'while'),
+    block: ($) =>
+      prec(
+        3, repeat1(
+          choice(
+            field('comment', $.comment),
+            $.expression,
+            $._statements,
+            $.function_call,
+            $.keyword))),
 
     identifier: ($) => /[a-zA-Z_]+[a-zA-Z0-9_]*/,
+
     factor: ($) =>
-      prec.right(choice($._number, $.string, $.identifier, $.operation, $.function_call)),
+      prec.right(seq(choice($.number,
+        $.string,
+        $.identifier,
+        $.operation,
+        $.function_call,
+        $.range), optional($._end_of_line))),
+
+    range: ($) =>
+      seq(
+        $._range_element, ':', $._range_element,
+        optional(seq(':', $._range_element))
+      ),
+
+    _range_element: ($) =>
+      choice($.identifier, $.number, $.function_call),
+
     function_call: ($) =>
       prec.left(
         3,
@@ -103,11 +221,16 @@ module.exports = grammar({
 
     string: ($) => seq($._single_quote, /([^']|(''))*/, $._single_quote),
 
+    keyword: ($) => seq(choice('return', 'continue', 'break'), optional($._end_of_line)),
+    _return: (_) => 'return',
+    _break: (_) => 'break',
+    _continue: (_) => 'continue',
+
     _single_quote: (_) => '\'',
     _semi_colon: ($) => ';',
     _eq: ($) => '=',
     _operator: ($) => new RegExp('[+\\-*/%\\^:<>]'),
-    _number: ($) => /\d+/g,
+    number: ($) => /\d+/g,
     end: ($) => 'end',
     function_keyword: ($) => 'function',
     vector_definition: ($) =>
@@ -116,11 +239,11 @@ module.exports = grammar({
       seq('{', repeat(seq($.factor, optional(choice(',', ';')))), '}'),
     _and: ($) => '&&',
     _or: ($) => '||',
-    _not: ($) => '!',
-    _diff: ($) => '!=',
+    _not: ($) => '~',
+    _diff: ($) => '~=',
     _comparator_equal: ($) => '==',
     _bool_keywords: ($) => choice('true', 'false'),
     comment: ($) => seq('%', /.+/, '\n'),
-    _end_of_line: ($) => choice(';', '\n', '\r'),
+    _end_of_line: ($) => choice(';', '\n', '\r', ','),
   },
 });
--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,13 +1,67 @@
-; highlights.scm
+  ; highlights.scm
 
-function_keyword: (identifier) @keyword
+function_keyword: (function_keyword) @keyword.function
+
+(function_definition
+function_name: (identifier) @function
+(end) @function)
+
+(parameter_list (identifier) @parameter)
+
+[
+    "if"
+    "elseif"
+    "else"
+    "switch"
+    "case"
+    "otherwise"
+] @conditional
+
+(if_statement (end) @conditional)
+(switch_statement (end) @conditional)
+
+["for" "while"] @repeat
+(for_statement (end) @repeat)
+(while_statement (end) @repeat)
+
+["try" "catch"] @exception
+(try_statement (end) @exception)
+
 (function_definition end: (end) @keyword)
-structure_keyword: (_) @keyword 
 
-"true" @constant.builtin
-"false" @constant.builtin
+["return" "break" "continue"] @keyword.return
 
-(identifier) @string
-return_variable: (return_value) @type.builtin
+(
+(identifier) @constant.builtin
+(#any-of? @constant.builtin "true" "false")
+)
 
+;; Punctuations
+
+[
+  ";"
+  ":"
+  ","
+] @punctuation.delimiter
+
+; ;; Brackets
+
+[
+ "("
+ ")"
+ "["
+ "]"
+ "{"
+ "}"
+] @punctuation.bracket
+
+;; Number
+(number) @number
+
+;; String
 (string) @string
+(string) @spell
+
+;; Comment
+(comment) @comment
+(comment) @spell
